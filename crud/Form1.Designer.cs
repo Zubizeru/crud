9994a2ec46bb1f18a5f5d57a6f6d5c10a4cec3e5
Zubiseru <jose.ehmendes@senacsp.edu.br>
2025-04-15 16:47:21 -0300

    crud atualizado

---
+++ crud/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace crud
+{
+    partial class FrmCadastroDeCliente
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTopo = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.tbCadastro = new System.Windows.Forms.TabPage();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.txtCPF = new System.Windows.Forms.MaskedTextBox();
+            this.lblCPF = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtNomeSocial = new System.Windows.Forms.TextBox();
+            this.lblNomeSocial = new System.Windows.Forms.Label();
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.lblNomeCompleto = new System.Windows.Forms.Label();
+            this.tbConsulta = new System.Windows.Forms.TabPage();
+            this.lstCliente = new System.Windows.Forms.ListView();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnNovoCliente = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.panelTopo.SuspendLayout();
+            this.TabControl.SuspendLayout();
+            this.tbCadastro.SuspendLayout();
+            this.tbConsulta.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTopo
+            // 
+            this.panelTopo.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panelTopo.Controls.Add(this.lblTitulo);
+            this.panelTopo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopo.Location = new System.Drawing.Point(0, 0);
+            this.panelTopo.Name = "panelTopo";
+            this.panelTopo.Size = new System.Drawing.Size(933, 100);
+            this.panelTopo.TabIndex = 0;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(304, 42);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(221, 25);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Cadastro de clientes";
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.tbCadastro);
+            this.TabControl.Controls.Add(this.tbConsulta);
+            this.TabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TabControl.Location = new System.Drawing.Point(12, 106);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(909, 332);
+            this.TabControl.TabIndex = 1;
+            // 
+            // tbCadastro
+            // 
+            this.tbCadastro.Controls.Add(this.txtCPF);
+            this.tbCadastro.Controls.Add(this.lblCPF);
+            this.tbCadastro.Controls.Add(this.txtEmail);
+            this.tbCadastro.Controls.Add(this.lblEmail);
+            this.tbCadastro.Controls.Add(this.txtNomeSocial);
+            this.tbCadastro.Controls.Add(this.lblNomeSocial);
+            this.tbCadastro.Controls.Add(this.txtNomeCompleto);
+            this.tbCadastro.Controls.Add(this.lblNomeCompleto);
+            this.tbCadastro.Location = new System.Drawing.Point(4, 29);
+            this.tbCadastro.Name = "tbCadastro";
+            this.tbCadastro.Padding = new System.Windows.Forms.Padding(3);
+            this.tbCadastro.Size = new System.Drawing.Size(901, 299);
+            this.tbCadastro.TabIndex = 0;
+            this.tbCadastro.Text = "Dados de clientes";
+            this.tbCadastro.UseVisualStyleBackColor = true;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.Color.White;
+            this.btnSalvar.Location = new System.Drawing.Point(16, 440);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(127, 42);
+            this.btnSalvar.TabIndex = 9;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCPF.Location = new System.Drawing.Point(155, 152);
+            this.txtCPF.Mask = "000,000,000-00";
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(121, 26);
+            this.txtCPF.TabIndex = 8;
+            // 
+            // lblCPF
+            // 
+            this.lblCPF.AutoSize = true;
+            this.lblCPF.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblCPF.Location = new System.Drawing.Point(101, 154);
+            this.lblCPF.Name = "lblCPF";
+            this.lblCPF.Size = new System.Drawing.Size(44, 20);
+            this.lblCPF.TabIndex = 6;
+            this.lblCPF.Text = "CPF:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Location = new System.Drawing.Point(155, 108);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(340, 26);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblEmail.Location = new System.Drawing.Point(93, 110);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(52, 20);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtNomeSocial
+            // 
+            this.txtNomeSocial.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNomeSocial.Location = new System.Drawing.Point(155, 64);
+            this.txtNomeSocial.Name = "txtNomeSocial";
+            this.txtNomeSocial.Size = new System.Drawing.Size(340, 26);
+            this.txtNomeSocial.TabIndex = 3;
+            // 
+            // lblNomeSocial
+            // 
+            this.lblNomeSocial.AutoSize = true;
+            this.lblNomeSocial.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblNomeSocial.Location = new System.Drawing.Point(43, 66);
+            this.lblNomeSocial.Name = "lblNomeSocial";
+            this.lblNomeSocial.Size = new System.Drawing.Size(102, 20);
+            this.lblNomeSocial.TabIndex = 2;
+            this.lblNomeSocial.Text = "Nome Social:";
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNomeCompleto.Location = new System.Drawing.Point(155, 20);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(340, 26);
+            this.txtNomeCompleto.TabIndex = 1;
+            // 
+            // lblNomeCompleto
+            // 
+            this.lblNomeCompleto.AutoSize = true;
+            this.lblNomeCompleto.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblNomeCompleto.Location = new System.Drawing.Point(18, 22);
+            this.lblNomeCompleto.Name = "lblNomeCompleto";
+            this.lblNomeCompleto.Size = new System.Drawing.Size(127, 20);
+            this.lblNomeCompleto.TabIndex = 0;
+            this.lblNomeCompleto.Text = "Nome Completo:";
+            // 
+            // tbConsulta
+            // 
+            this.tbConsulta.Controls.Add(this.btnReset);
+            this.tbConsulta.Controls.Add(this.lstCliente);
+            this.tbConsulta.Controls.Add(this.btnPesquisar);
+            this.tbConsulta.Controls.Add(this.txtBuscar);
+            this.tbConsulta.Location = new System.Drawing.Point(4, 29);
+            this.tbConsulta.Name = "tbConsulta";
+            this.tbConsulta.Padding = new System.Windows.Forms.Padding(3);
+            this.tbConsulta.Size = new System.Drawing.Size(901, 299);
+            this.tbConsulta.TabIndex = 1;
+            this.tbConsulta.Text = "Consulta";
+            this.tbConsulta.UseVisualStyleBackColor = true;
+            // 
+            // lstCliente
+            // 
+            this.lstCliente.HideSelection = false;
+            this.lstCliente.Location = new System.Drawing.Point(22, 39);
+            this.lstCliente.MultiSelect = false;
+            this.lstCliente.Name = "lstCliente";
+            this.lstCliente.Size = new System.Drawing.Size(873, 252);
+            this.lstCliente.TabIndex = 2;
+            this.lstCliente.UseCompatibleStateImageBehavior = false;
+            this.lstCliente.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lstCliente_ItemSelectionChanged);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnPesquisar.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.btnPesquisar.FlatAppearance.BorderSize = 0;
+            this.btnPesquisar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPesquisar.ForeColor = System.Drawing.Color.White;
+            this.btnPesquisar.Location = new System.Drawing.Point(727, 6);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(129, 30);
+            this.btnPesquisar.TabIndex = 1;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(22, 8);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(695, 26);
+            this.txtBuscar.TabIndex = 0;
+            // 
+            // btnNovoCliente
+            // 
+            this.btnNovoCliente.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnNovoCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovoCliente.ForeColor = System.Drawing.Color.White;
+            this.btnNovoCliente.Image = global::crud.Properties.Resources.cliente;
+            this.btnNovoCliente.Location = new System.Drawing.Point(149, 440);
+            this.btnNovoCliente.Name = "btnNovoCliente";
+            this.btnNovoCliente.Size = new System.Drawing.Size(127, 42);
+            this.btnNovoCliente.TabIndex = 10;
+            this.btnNovoCliente.Text = "Novo Cliente";
+            this.btnNovoCliente.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnNovoCliente.UseVisualStyleBackColor = false;
+            this.btnNovoCliente.Click += new System.EventHandler(this.btnNovoCliente_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.White;
+            this.btnReset.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.btnReset.FlatAppearance.BorderSize = 2;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.ForeColor = System.Drawing.Color.Wheat;
+            this.btnReset.Image = global::crud.Properties.Resources.icons8_clear_25;
+            this.btnReset.Location = new System.Drawing.Point(862, 6);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(30, 30);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // FrmCadastroDeCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 487);
+            this.Controls.Add(this.btnNovoCliente);
+            this.Controls.Add(this.TabControl);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.panelTopo);
+            this.Name = "FrmCadastroDeCliente";
+            this.Text = "Form1";
+            this.panelTopo.ResumeLayout(false);
+            this.panelTopo.PerformLayout();
+            this.TabControl.ResumeLayout(false);
+            this.tbCadastro.ResumeLayout(false);
+            this.tbCadastro.PerformLayout();
+            this.tbConsulta.ResumeLayout(false);
+            this.tbConsulta.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTopo;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage tbCadastro;
+        private System.Windows.Forms.TabPage tbConsulta;
+        private System.Windows.Forms.MaskedTextBox txtCPF;
+        private System.Windows.Forms.Label lblCPF;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtNomeSocial;
+        private System.Windows.Forms.Label lblNomeSocial;
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label lblNomeCompleto;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.ListView lstCliente;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnNovoCliente;
+    }
+}
+
---
+++ crud/Form1.cs
@@ -0,0 +1,264 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace crud
+{
+    public partial class FrmCadastroDeCliente : Form
+    {
+        //Conexao com o banco de dados mySql
+        MySqlConnection Conexao;
+        string data_source = "datasource=localhost;username=root;password =; database=bd_cadastro";
+
+        private int? codigo_cliente = null;
+
+        public FrmCadastroDeCliente()
+        {
+            InitializeComponent();
+
+            //Configurações inicial do listview
+            lstCliente.View = View.Details;
+            lstCliente.LabelEdit = true;
+            lstCliente.AllowColumnReorder = true;
+            lstCliente.FullRowSelect = true;
+            lstCliente.GridLines = true;
+
+            //Definindo as colunas do ListView
+            lstCliente.Columns.Add("Codigo", 100, HorizontalAlignment.Left);
+            lstCliente.Columns.Add("Nome Completo", 200, HorizontalAlignment.Left);
+            lstCliente.Columns.Add("Nome Social", 200, HorizontalAlignment.Left);
+            lstCliente.Columns.Add("E-mail", 200, HorizontalAlignment.Left);
+            lstCliente.Columns.Add("CPF", 200, HorizontalAlignment.Left);
+
+            carregarClientes();
+        }
+
+        private void carregar_clientes_com_query(string query)
+        {
+            try
+            {
+                // Cria a conexao com o banco de dados
+                Conexao = new MySqlConnection(data_source);
+                Conexao.Open();
+
+                // Executa a consulta SQL fornecida
+                MySqlCommand cmd = new MySqlCommand(query, Conexao);
+
+                // Se a consulta contém o parametro @q, adiciona o valor da caixa de pesquisa
+                if (query.Contains("@q"))
+                {
+                    cmd.Parameters.AddWithValue("@q", "%" + txtBuscar.Text + "%");
+                }
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                lstCliente.Items.Clear();
+
+                //Preenche o ListView com os dados dos clientes
+                while (reader.Read())
+                {
+                    string[] row =
+                    {
+                        Convert.ToString(reader.GetInt32(0)),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                        reader.GetString(4)
+                    };
+                    lstCliente.Items.Add(new ListViewItem(row));
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Erro " + ex.Number + " ocorreu: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (Conexao != null && Conexao.State == ConnectionState.Open)
+                {
+                    Conexao.Close();
+                    // MessageBox.Show("Conexão Fechada");
+                }
+            }
+        }
+
+        private void carregarClientes()
+        {
+            string query = "SELECT * FROM dadosdecliente ORDER BY codigo DESC";
+            carregar_clientes_com_query(query);
+        }
+
+        private bool isValidEmail(string email)
+        {
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(email);
+        }
+
+        private bool isValidCPF(string cpf)
+        {
+            cpf = cpf.Replace(",", "").Replace("-", "").Replace(".", "");
+
+            if (cpf.Length != 11 || !cpf.All(char.IsDigit))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtNomeCompleto.Text.Trim()) ||
+                    string.IsNullOrEmpty(txtEmail.Text.Trim()) ||
+                    string.IsNullOrEmpty(txtCPF.Text.Trim()))
+                {
+                    MessageBox.Show("Todos os campos devem ser preenchidos", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                string cpf = txtCPF.Text.Replace(".", "").Replace("-", "").Replace(",", "").Trim();
+                if (!isValidCPF(cpf))
+                {
+                    MessageBox.Show("CPF inválido. Certifique-se de que o CPF tenha 11 dígitos numéricos", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                string email = txtEmail.Text.Trim();
+                if (!isValidEmail(email))
+                {
+                    MessageBox.Show("E-mail inválido. Certifique-se de que o e-mail está no formato correto", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                Conexao = new MySqlConnection(data_source);
+                Conexao.Open();
+                // MessageBox.Show("Conexão Aberta");
+
+                MySqlCommand cmd = new MySqlCommand
+                {
+                    Connection = Conexao
+                };
+
+                cmd.Prepare();
+
+                if (codigo_cliente == null)
+                {
+                    // insert CREATE
+                    cmd.CommandText = "INSERT INTO dadosdecliente(nomecompleto, nomesocial, email, cpf)" +
+                    "Values (@nomecompleto, @nomesocial, @email, @cpf)";
+
+                    cmd.Parameters.AddWithValue(@"nomecompleto", txtNomeCompleto.Text.Trim());
+                    cmd.Parameters.AddWithValue(@"nomesocial", txtNomeSocial.Text.Trim());
+                    cmd.Parameters.AddWithValue(@"email", email);
+                    cmd.Parameters.AddWithValue(@"cpf", cpf);
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Contato inserido com sucesso: ", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    // UPDATE 
+                    cmd.CommandText = $"UPDATE `dadosdecliente` SET " +
+                    $"nomecompleto = @nomecompleto, " +
+                    $"nomesocial = @nomesocial, " +
+                    $"email = @email, " +
+                    $"cpf = @cpf " +   
+                    $"WHERE codigo = @codigo ";
+
+                    cmd.Parameters.AddWithValue("@nomecompleto", txtNomeCompleto.Text.Trim());
+                    cmd.Parameters.AddWithValue("@nomesocial", txtNomeSocial.Text.Trim());
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@cpf", cpf);
+                    cmd.Parameters.AddWithValue("@codigo", codigo_cliente);
+
+                    //Executa o comando de alteração no banco
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show($"Os dados com o código {codigo_cliente} foram alterados com Sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Erro " + ex.Number + " ocorreu: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (Conexao != null && Conexao.State == ConnectionState.Open)
+                {
+                    Conexao.Close();
+                    // MessageBox.Show("Conexão Fechada");
+                }
+            }
+
+            carregarClientes();
+            limparCampos();
+
+            TabControl.SelectedIndex = 1;
+        }
+
+        private void limparCampos()
+        {
+            txtNomeCompleto.Text = "";
+            txtNomeSocial.Text = "";
+            txtEmail.Text = "";
+            txtCPF.Text = "";
+            codigo_cliente = null;
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM dadosdecliente WHERE nomecompleto LIKE @q OR nomesocial LIKE @q ORDER BY codigo DESC";
+            carregar_clientes_com_query(query);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtBuscar.Text = "";
+            string query = "SELECT * FROM dadosdecliente ORDER BY codigo DESC";
+            carregar_clientes_com_query(query);
+        }
+
+        private void btnNovoCliente_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+
+            txtNomeCompleto.Focus();
+        }
+
+        private void lstCliente_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            ListView.SelectedListViewItemCollection clientedaselecao = lstCliente.SelectedItems;
+
+            foreach (ListViewItem item in clientedaselecao)
+            {
+                codigo_cliente = Convert.ToInt32(item.SubItems[0].Text);
+
+                //Exibe uma MessageBox com o código do cliente
+                MessageBox.Show($"Código do Cliente: {codigo_cliente.ToString()}", "Código Selecionado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtNomeCompleto.Text = item.SubItems[1].Text;
+                txtNomeSocial.Text = item.SubItems[2].Text;
+                txtEmail.Text = item.SubItems[3].Text;
+                txtCPF.Text = item.SubItems[4].Text;
+            }
+
+            // Muda para a aba de dados do cliente
+            TabControl.SelectedIndex = 0;
+        }
+    }
+}
